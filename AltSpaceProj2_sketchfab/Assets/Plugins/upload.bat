2bd2e1b915a1a9036fd2c3489adf445da4536ed3
Ying <ying@taro.localdomain>
2020-09-07 20:24:27 -0700

    Project 2 prefab init commit

---
+++ AltSpaceProj2_sketchfab/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/ying/altspace/AltSpaceProj2_sketchfab/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1550307961119703752.json
